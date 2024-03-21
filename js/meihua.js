9a9eaa61c629e7fc2fff513a6e25a75abc5f2fe7
wytgm <2971837355@qq.com>
2024-03-21 22:37:42 +0800

    Site updated: 2024-03-21 22:37:29

--- js/meihua.js
+++ js/meihua.js
@@ -204,10 +204,10 @@ function createWinbox() {
 
 <h2><center>字体设置</center></h2>
 <p id="swfs">
-<a class="swf" id="swf_ZhuZiAWan" href="javascript:;" rel="noopener external nofollow" style="font-family:'ZhuZiAWan'!important;color:black" onclick="setFont('ZhuZiAWan')">筑紫A丸标准体2.0</a>
-<a class="swf" id="swf_漫画体"  href="javascript:;" rel="noopener external nofollow" style="font-family:'漫画体'!important;color:black" onclick="setFont('漫画体')">漫画体</a>
-<a class="swf" id="swf_也字工厂弗吉亚体"  href="javascript:;" rel="noopener external nofollow" style="font-family:'也字工厂弗吉亚体'!important;color:black" onclick="setFont('也字工厂弗吉亚体')">也字工厂弗吉亚体</a>
-<a class="swf" id="swf_default" href="javascript:;" rel="noopener external nofollow" style="font-family:-apple-system, IBM Plex Mono ,monosapce,'微软雅黑', sans-serif;!important;color:black" onclick="setFont('default')">系统默认</a>
+<a class="swf" sytle="margin-right:10px" id="swf_ZQFS" href="javascript:;" rel="noopener external nofollow" style="font-family:'ZQFS'!important;color:black" onclick="setFont('ZQFS')">朱雀仿宋</a>
+<a class="swf" sytle="margin-right:10px" id="swf_comic"  href="javascript:;" rel="noopener external nofollow" style="font-family:'comic'!important;color:black" onclick="setFont('comic')">漫画体</a>
+<a class="swf" sytle="margin-right:10px" id="swf_FJY"  href="javascript:;" rel="noopener external nofollow" style="font-family:'FJY'!important;color:black" onclick="setFont('FJY')">也字工厂弗吉亚体</a>
+<a class="swf" sytle="margin-right:10px" id="swf_default" href="javascript:;" rel="noopener external nofollow" style="font-family:-apple-system, IBM Plex Mono ,monosapce,'微软雅黑', sans-serif;!important;color:black" onclick="setFont('default')">系统默认</a>
 
 
 
--- js/yaoyaole.js
+++ js/yaoyaole.js
@@ -1,14 +1,4 @@
-// const github = SakanaWidget.getCharacter('chisato');
-// github.image = `https://raw.githubusercontent.com/dsrkafuu/sakana-widget/main/docs/github.png`;
-// SakanaWidget.registerCharacter('github', github);
-// new SakanaWidget({ character: 'github' }).mount('#sakana-widget');
-
-const xing01 = SakanaWidget.getCharacter('star');
-xing01.image = `https://fastly.jsdelivr.net/gh/wytgm/BlogImg/图标/xingeatgua.png`;
-SakanaWidget.registerCharacter('xing01', xing01);
-new SakanaWidget({ character: 'xing01' }).mount('#sakana-widget');
-
-// const github = SakanaWidget.getCharacter('chisato');
-// github.image = `https://raw.githubusercontent.com/dsrkafuu/sakana-widget/main/docs/github.png`;
-// SakanaWidget.registerCharacter('github', github);
-// new SakanaWidget({ character: 'github' }).mount('#sakana-widget');
+const github = SakanaWidget.getCharacter('chisato');
+github.image = `https://raw.githubusercontent.com/wytgm/BlogImg/master/%E5%9B%BE%E6%A0%87/xingeatgua.png`;
+SakanaWidget.registerCharacter('github', github);
+new SakanaWidget({ character: 'github' }).mount('#sakana-widget');
